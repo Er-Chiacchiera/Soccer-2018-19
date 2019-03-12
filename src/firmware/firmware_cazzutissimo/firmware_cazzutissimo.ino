8894b533bafc5e8198664bb6c5d102f4fcc60915
beth <francesco.spena4@gmail.com>
2019-03-12 20:39:56 +0100

    src

--- src/firmware/firmware_cazzutissimo/firmware_cazzutissimo.ino
+++ src/firmware/firmware_cazzutissimo/firmware_cazzutissimo.ino
@@ -24,7 +24,7 @@
 #include "ares_imu.h"
 #include "ares_timer.h"
 #include "ares_rullo.h"
-//#include "ares_encoders.h"
+#include "ares_encoders.h"
 
 
 void setup() {
@@ -47,9 +47,11 @@ void setup() {
   }
   delay(1000);
   PhoenixImu_handle(&imu);
-  PhoenixImu_setOffset(&imu, imu.heading_attuale);
+  PhoenixImu_setOffset(&imu, imu.heading_attuale);*/
+
+  Encoder_init();
  
- while(PhoenixImu_init(&imu)) {
+ /*while(PhoenixImu_init(&imu)) {
   delay(500);
   Serial.println("non funziona");
  }
@@ -68,7 +70,46 @@ void setup() {
   PhoenixLineHandler_init(&line_handler, line_sensors);
   Serial.println("Line Handler initialized...");*/
 }
+
+void Test_connections(){
+  PhoenixJoint_handle(&joints[0]);
+  PhoenixJoint_setSpeed(&joints[0], 255);
+  //Serial.println("MOTORE 0 (ALTO A DESTRA)");
+  delay(1000);
+  PhoenixJoint_handle(&joints[1]);
+  PhoenixJoint_setSpeed(&joints[1], 255);
+  //Serial.println("MOTORE 1 (ALTO A SINISTRA)");
+  delay(1000);
+  PhoenixJoint_handle(&joints[2]);
+  PhoenixJoint_setSpeed(&joints[2], 255);
+  //Serial.println("MOTORE 0 (BASSO)");
+  delay(1000);
+}
   
+void Test_ImuPid(){
+  PhoenixImu_handle(&imu);
+  PhoenixDrive_setSpeed(&drive , 0,0,-imu.output_pid/180);
+  PhoenixDrive_handle(&drive);
+  Serial.print(imu.heading_target);
+  Serial.print("\t");
+  Serial.print(imu.sum_i);
+  Serial.print("\t");
+  Serial.print(imu.output_pid/180);
+  Serial.println("\t");
+  delay(10);
+}
+
+void Test_Line(){
+  for(int i=0;i<NUM_LINE_SENSORS;i++){
+  PhoenixLineSensor_handle(&line_sensors[i]);
+  Serial.println(line_sensors[i].misura); }
+}
+
+void TestRullo(){
+  PhoenixRullo_start();
+  PhoenixDrive_setSpeed(&drive, 0,1,0);
+  PhoenixDrive_handle(&drive);
+}
 /**
  * avanti = 0, 1, 0      per toccare la vel_max imposta a 2
  * indietro = 0, -1, 0
@@ -85,52 +126,12 @@ void setup() {
  */
 
 void loop() {
-
-  //PROVA CONNESSIONI MOTORI 
-/*
-  PhoenixJoint_handle(&joints[0]);
-  PhoenixJoint_setSpeed(&joints[0], 255);
-  PhoenixJoint_setSpeed(&joints[0], -255);
-  delay(500);
-  PhoenixJoint_handle(&joints[1]);
-  PhoenixJoint_setSpeed(&joints[1], 255);
-  PhoenixJoint_setSpeed(&joints[1], -255);
-  delay(500);
-  PhoenixJoint_handle(&joints[2]);
-  PhoenixJoint_setSpeed(&joints[2], 255);
-  PhoenixJoint_setSpeed(&joints[2], -255);
-  delay(500);
-
-*/ 
-  //IMU
-  /*
-  PhoenixImu_handle(&imu);
-  PhoenixDrive_setSpeed(&drive , 0,0,-imu.output_pid/180);
-  PhoenixDrive_handle(&drive);
-  Serial.print(imu.heading_target);
-  Serial.print("\t");
-  Serial.print(imu.sum_i);
-  Serial.print("\t");
-  Serial.print(imu.output_pid/180);
-  Serial.println("\t");
-  delay(10);
-*/
-  //PhoenixRullo_start();
-
-
-  //RULLO*/
-  /*
-  PhoenixRullo_start();
-  PhoenixDrive_setSpeed(&drive, 0,1,0);
-  PhoenixDrive_handle(&drive);
-*/
-
-  //LINEEE LETTURA 
-/*
-  for(int i=0;i<NUM_LINE_SENSORS;i++){
-  PhoenixLineSensor_handle(&line_sensors[i]);
-  Serial.println(line_sensors[i].misura); 
-  }*/
-
   
+  Encoder_sample();
+  Serial.print(Encoder_getValue(0));
+  Serial.print("\t");
+  Serial.print(Encoder_getValue(1));
+  Serial.print("\t");
+  Serial.print(Encoder_getValue(2));
+  Serial.println("\t");
 }
