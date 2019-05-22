9af19446f3be9dd6fd1f3b6e12ddefd2c681d73f
Unknown <francesco.spena4@gmail.com>
2019-05-22 17:38:10 +0200

    delete

--- src/firmware_vecchio/ares_globals.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Adafruit_ADS1015.h>   //includo librerie 
-#include <Arduino.h>
-#define NUM_LINEDETECTORS 16   //dichiaro il numero di sensori 
-
-extern Adafruit_ADS1015 line_adc[4];   //richiamo funzioni dalle librerie di Arduino
-extern AresLineDetector line_detectors[NUM_LINEDETECTORS];
--- src/portiere/firmware_cazzutissimo_portiere/ares_params.h
+++
@@ -1,8 +0,0 @@
-/**
- * phoenix_params.h
- **/
-#pragma once
-
-#define NUM_JOINTS 3
-#define NUM_LINE_SENSORS 5
-#define SOLENOIDE 255
